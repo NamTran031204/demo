fdeb7f58073c184ad8bb0350ba5ef1b686a2357a
Nam031204 <tpnam1w3@gmail.com>
2024-05-12 11:13:35 +0700

    hehe

--- codemoi.cpp
+++ codemoi.cpp
@@ -1,8 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+double calculatePi() {
+  double pi = 0.0;
+  int sign = 1;
+  for (int i = 0; i < 1000000; i++) {
+    pi += sign * (1.0 / (2 * i + 1));
+    sign *= -1;
+  }
+  pi *= 4;
+  return pi;
+}
 int main(){
     int n=4;
-    cout<< n <<endl;
-    cout<<"nam dep trai nhat qua dat";
-    return 0;
+    cout<< calculatePi()*n <<endl;
 }
